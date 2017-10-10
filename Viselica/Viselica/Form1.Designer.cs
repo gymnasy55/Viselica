41789bcd05cfd2416a1f51e08ec7f71fbed38109
gymnasy55 <danyarak7@gmail.com>
2017-10-10 17:18:22 +0300

    0.1

---
+++ Viselica/Viselica/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace Viselica
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ImageBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ImageBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ImageBox1
+            // 
+            this.ImageBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("ImageBox1.BackgroundImage")));
+            this.ImageBox1.Location = new System.Drawing.Point(553, 243);
+            this.ImageBox1.Name = "ImageBox1";
+            this.ImageBox1.Size = new System.Drawing.Size(190, 260);
+            this.ImageBox1.TabIndex = 0;
+            this.ImageBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(755, 515);
+            this.Controls.Add(this.ImageBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ImageBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ImageBox1;
+    }
+}
+
---
+++ Viselica/Viselica/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Viselica
+{
+    public partial class Form1 : Form
+    {
+        string correct, str;
+        int x, y, counter;
+        Button[] bt;
+        public Form1()
+        {
+            InitializeComponent();
+            correct = "ПРИВЕТ+";
+            str = "ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ";
+            x = 2;
+            y = this.Height - 210;
+        }
+
+        public void Form1_Load(object sender, EventArgs e)
+        {
+            bt = new Button[str.Length];
+            for (int j = 0; j < 3; j++)
+            {
+                x = 2;
+                for (int i = 0; i < str.Length / 3; i++)
+                {
+                    bt[j * 11 + i] = new Button();
+                    bt[j * 11 + i].Name = "button" + (j * 11 + i).ToString();
+                    bt[j * 11 + i].Text = str[j * 11 + i].ToString();
+                    bt[j * 11 + i].Left = x;
+                    bt[j * 11 + i].Top = y;
+                    bt[j * 11 + i].Width = 40;
+                    bt[j * 11 + i].Height = 40;
+                    bt[j * 11 + i].Font = new System.Drawing.Font("Microsoft Sans Serif",
+                                                                  14F,
+                                                                  System.Drawing.FontStyle.Bold,
+                                                                  System.Drawing.GraphicsUnit.Point,
+                                                                  ((byte)(204)));
+                    x += 50;
+                    bt[j * 11 + i].Click += bt_Click;
+                    this.Controls.Add(bt[j * 11 + i]);
+                }
+                y += 50;
+            }
+        }
+        private Bitmap MyImage;
+        public void bt_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            btn.Enabled = false;
+            counter++;
+            switch (counter) //КОЛЯ, СДЕЛАЙ КАРТИНКИ
+            {
+                case 0:
+                    MyImage = new Bitmap("1.jpg");
+                    ImageBox1.Image = MyImage;
+                    break;
+                case 1:
+                    MyImage = new Bitmap("2.jpg");
+                    ImageBox1.Image = MyImage;
+                    break;
+                case 2:
+                    MyImage = new Bitmap("3.jpg");
+                    ImageBox1.Image = MyImage;
+                    break;
+                case 3:
+                    MyImage = new Bitmap("1.jpg");
+                    ImageBox1.Image = MyImage;
+                    break;
+                case 4:
+                    MyImage = new Bitmap("2.jpg");
+                    ImageBox1.Image = MyImage;
+                    break;
+                case 5:
+                    MyImage = new Bitmap("3.jpg");
+                    ImageBox1.Image = MyImage;
+                    MessageBox.Show("Вы проиграли");
+                    break;
+            }
+        }
+    }
+}
